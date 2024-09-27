fa5880dfc14f5a979e4c239073b4ce18c1f432c2
Bouyou34 <34380414+Bouyou34@users.noreply.github.com>
2024-09-27 16:57:44 +0200

    start integrating logging start integrating storage util

--- packages/modulife_todos/lib/repositories/todo_repository.dart
+++ packages/modulife_todos/lib/repositories/todo_repository.dart
@@ -1,34 +1,48 @@
 import 'dart:convert';
-import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:modulife_todos/models/todo.dart';
+
+import 'package:modulife_utils/modulife_utils.dart';
 
 class TodoRepository {
   static const String todosKey = 'todos_key';
 
+  /// Save the list of todos to storage
   Future<void> saveTodos(List<Todo> todos) async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    final String todosJson =
-        jsonEncode(todos.map((Todo todo) => todo.toMap()).toList());
+    try {
+      final String todosJson =
+          jsonEncode(todos.map((Todo todo) => todo.toMap()).toList());
 
-    await prefs.setString(todosKey, todosJson);
+      await StorageUtils().saveString(todosKey, todosJson);
+    } catch (e) {
+      LogService.e('Failed to save todos', e);
+    }
   }
 
+  /// Load the list of todos from storage
   Future<List<Todo>> loadTodos() async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    final String? todosJson = prefs.getString(todosKey);
+    try {
+      final String? todosJson = StorageUtils().getString(todosKey);
 
-    if (todosJson != null) {
-      final List<dynamic> jsonList = jsonDecode(todosJson);
-      return jsonList.map((dynamic json) => Todo.fromMap(json)).toList();
-    } else {
+      if (todosJson != null) {
+        final List<dynamic> jsonList = jsonDecode(todosJson);
+        return jsonList.map((dynamic json) => Todo.fromMap(json)).toList();
+      } else {
+        LogService.i('No todos found in storage');
+        return [];
+      }
+    } catch (e) {
+      LogService.e('Failed to load todos', e);
       return [];
     }
   }
 
+  /// Clear all todos from storage
   Future<void> clearTodos() async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-
-    await prefs.remove(todosKey);
+    try {
+      await StorageUtils().remove(todosKey);
+    } catch (e) {
+      LogService.e('Failed to clear todos', e);
+    }
   }
 }
